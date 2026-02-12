8bb15f4ea4a41db6be3982af34f3d22437097314
acstudies-lab <acstudies.net@gmail.com>
2026-02-12 02:13:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "To: Joanna Valeria Pineda? 💝",
+    pageTitle: "To: Joanna Valeria Pineda",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        bears: ['🙊', '🐻','🦆']                       // Cute bear emojis
     },
 
     // Questions and answers
